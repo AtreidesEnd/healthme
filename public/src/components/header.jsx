b938f507c190ccb30bb39fceacdce69b600d07fa
Jared Meek <jmeek13@gmail.com>
2017-09-25 22:37:54 -0700

    link to / from main logo

--- public/src/components/header.jsx
+++ public/src/components/header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = ({loggedIn}) => (
-  <div className='app-header'>
-    <div className='app-logo'>HealthMe</div>
-    <nav className='app-header-nav'>
-      <a href='#' className='app-header-nav-link'>Settings</a>
-      <a href='#' className='app-header-nav-link'>{loggedIn ? 'Logout' : 'Login'}</a>
+  <div className="app-header">
+    <Link to="/" className="app-logo">HealthMe</Link>
+    <nav className="app-header-nav">
+      <a href="#" className="app-header-nav-link">Settings</a>
+      <a href="#" className="app-header-nav-link">{loggedIn ? "Logout" : "Login"}</a>
     </nav>
   </div>
 );
