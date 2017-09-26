31f45a3aac16c86faf675fb25a9588aeb51e25d0
Jared Meek <jmeek13@gmail.com>
2017-09-26 01:01:00 -0700

    implemented redirect to root and passed it down from container app to the new-entry forms

--- public/src/components/app.jsx
+++ public/src/components/app.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import Header from './header.jsx';
 import Sidenav from './sidenav.jsx';
 import Journal from './journal.jsx';
@@ -7,21 +7,17 @@ import NewMeal from './forms/new-meal.jsx';
 import NewActivity from './forms/new-activity.jsx';
 import NewFeeling from './forms/new-feeling.jsx';
 import NewDaily from './forms/new-daily.jsx';
-// mock data for now
+
 const defaultNavs = [{label: 'Journal', icon:'create', link:'/'},
                {label: 'Trends', icon:'show_chart', link:'/trends'},
                {label: 'History', icon:'history', link:'/history'}];
-const entries = [{type: 'daily', date:'2017-09-25', time:'18:00:00',
-                  desc:'2 Huel shakes and some avocado, feeling mostly good, glad to have gotten a workout in',
-                  details: {sleep:7, move:30, water:3}},
-                {type: 'feeling', date:'2017-09-24', time:'21:00:00',
-                desc:'went out drinking for birthday, tired now and starting to feel hungover',
-                details: {phys: 4, emo: 8, ill:0}},
-                {type: 'activity', date:'2017-09-25', time:'12:00:00',
-                desc:'little bit of rowing just to tire myself out',
-                details: {type: 'cardio', int: 5, duration:30 }}];
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.RootRedirect = () => (<Redirect to="/"/>);
+  }
+
   render() {
     return (
       <Router>
@@ -32,11 +28,11 @@ export default class App extends Component {
               <Sidenav navs={defaultNavs} />
             </div>
             <div className='app-content-module'>
-              <Route exact path="/" render={props => (<Journal entries={entries} />)} />
-              <Route path="/new/daily" component={NewDaily} />
-              <Route path="/new/meal" component={NewMeal} />
-              <Route path="/new/activity" component={NewActivity} />
-              <Route path="/new/feeling" component={NewFeeling} />
+              <Route exact path="/" render={props => (<Journal />)} />
+              <Route path="/new/daily" render={props => (<NewDaily redirect={this.RootRedirect} />)} />
+              <Route path="/new/meal" render={props => (<NewMeal redirect={this.RootRedirect} />)} />
+              <Route path="/new/activity" render={props => (<NewActivity redirect={this.RootRedirect} />)} />
+              <Route path="/new/feeling" render={props => (<NewFeeling redirect={this.RootRedirect} />)} />
             </div>
           </div>
         </div>
