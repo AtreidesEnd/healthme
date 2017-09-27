a0e4496b68e628e5c41e57870b38335e7c298fe3
Jared Meek <jmeek13@gmail.com>
2017-09-26 17:12:55 -0700

    initial frontend work to integrate with trenddata api

--- public/src/components/trends.jsx
+++ public/src/components/trends.jsx
@@ -7,7 +7,7 @@ export default class Trends extends Component {
     super(props);
     this.state = {
       entries: [],
-      selectedOutcome: '',
+      selectedOutcome: null,
       outcomesDropdown: [],
     };
   }
@@ -36,17 +36,26 @@ export default class Trends extends Component {
       .catch(err => console.log('Error: ', err));
   }
 
+  getTrendData(user = 'user1') {
+    console.log('This state outcome is: ', this.state.selectedOutcome);
+    console.log('Getting trend data for: ', user, ' and outcome: ', this.state.selectedOutcome);
+    axios.get('/api/trenddata', {params: {user: user, outcome: this.state.selectedOutcome, includeRaw: true}})
+      .then(resp => {
+        console.log('TrendData Response: ', resp);
+      }).catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="journal-container shadow">
         <div className="journal-header">
-          <span className="trends-header-title">What's behind my: </span>
+          <div className="trends-header-title">What's behind my: </div>
           <DropdownList id="outcome-select" name="outcome-select" className="trends-outcome-select"
             value={this.state.selectedOutcome} data={this.state.outcomes}
             textField="text" groupBy="group"
-            onChange={sel => {
-              console.log(sel.text);
-              this.setState({selectedOutcome: sel.text});
+            onChange={selectedOutcome => {
+              console.log(selectedOutcome);
+              this.setState({selectedOutcome}, this.getTrendData);
             }} />
         </div>
         <div className="trends-table-container">Trends table goes here - woohoo</div>
@@ -54,3 +63,27 @@ export default class Trends extends Component {
     );
   }
 }
+
+const TrendTable = () => {
+  return (
+    <div className="trend-table-container">
+      <table class="trend-table mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+        <thead>
+          <tr>
+            <th class="trend-table-header mdl-data-table__cell--non-numeric">Material</th>
+            <th>Quantity</th>
+            <th>Unit price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="trend-table-data mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+            <td>25</td>
+            <td>$2.90</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+
+};
